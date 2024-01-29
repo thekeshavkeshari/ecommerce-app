4ad772e982538e40e8f859c0614e1622e1c92e95
keshav keshari <kg1718129@gmail.com>
2024-01-30 04:07:52 +0530

    Sorting functionality achieved

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -7,6 +7,7 @@ import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
 import CircularProgress from "@mui/material/CircularProgress";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { enqueueSnackbar } from "notistack";
 
 function valuetext(value) {
   return `${value}°C`;
@@ -106,21 +107,20 @@ export default function HomePage() {
     }
   };
 
-  // useEffect(() => {
-  //   if (checked.length || value.length) filterProduct();
-  // }, [checked, value]);
+  //Handle Sort
+  const handleSort = ()=>{
+    const sortedProducts = [...product]; // Create a new array to avoid mutating the original
+    sortedProducts.sort((a, b) => a.price - b.price);
 
-  //life cycle method
-  // useEffect(() => {
-  //   getTotal();
-  //   if (!checked.length){ getAllProducts()};
-  //   //eslint-disable-next-line
-  // }, []);
+    setProduct(sortedProducts);
+    enqueueSnackbar("Product has been sorted", { variant: "info" });
+  }
 
+  
   useEffect(() => {
     getTotal();
     getAllProducts();
-
+    
     //eslint-disable-next-line
   }, []);
 
@@ -128,15 +128,15 @@ export default function HomePage() {
     try {
       const { data } = await axios.get(
         "http://localhost:8080/api/v1/category/get-category"
-      );
-      // console.log(data.category);
-      setCategories(data.category);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    getCategory();
+        );
+        // console.log(data.category);
+        setCategories(data.category);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    useEffect(() => {
+      getCategory();
     //eslint-disable-next-line
   }, []);
 
@@ -144,7 +144,7 @@ export default function HomePage() {
     <Layout className="relative" title={"home"}>
       <div className="w-full min-h-full ">
         <div className="fixed  bottom-0 left-0 flex z-50 w-full gap-[1px]">
-          <button className="bg-white w-1/2 p-6 text-xl shadow">Sort</button>
+          <button className="bg-white w-1/2 p-6 text-xl shadow" onClick={handleSort}>Sort</button>
 
           <div className="bg-white w-1/2 text-xl text-center  shadow">
             <div className="relative w-full">
@@ -160,7 +160,7 @@ export default function HomePage() {
                             onChange={(e) =>
                               handleChecked(e.target.checked, element._id)
                             }
-                          />
+                            />
                           {element.name}
                           <br />
                         </div>
@@ -177,7 +177,7 @@ export default function HomePage() {
                         valueLabelDisplay="auto"
                         getAriaValueText={valuetext}
                         max={maxRange}
-                      />
+                        />
                     </Box>
                   </div>
                   <button onClick={() => window.location.reload()}>
@@ -190,9 +190,8 @@ export default function HomePage() {
               className="p-6"
               onClick={() => {
                 setFilter(!filter);
-                window.alert("filter applied");
               }}
-            >
+              >
               {!filter ? "Filters" : "Apply"}
             </button>
           </div>
@@ -211,12 +210,12 @@ export default function HomePage() {
                 <CircularProgress disableShrink />
               </div>
             }
-          >
+            >
             {product.map((element) => {
               return (
                 <Link
-                  key={element._id}
-                  to={`/dashboard/admin/product/${element.slug}`}
+                key={element._id}
+                to={`/dashboard/admin/product/${element.slug}`}
                 >
                   <div className="p-6 shadow-2xl rounded-2xl mb-2 h-full">
                     <div className="rounded-2xl aspect-square flex justify-center shadow-md mb-4">
@@ -225,7 +224,7 @@ export default function HomePage() {
                         className="max-h-full rounded-2xl text-center"
                         alt={element.slug}
                         loading="lazy"
-                      />
+                        />
                     </div>
                     <h3 className="text-3xl lg:text-2xl font-semibold lg:font-normal mb-2">
                       {element.name}
@@ -245,10 +244,10 @@ export default function HomePage() {
           </InfiniteScroll>
 
           {/* {spinner && (
-              <div className="w-full flex justify-center">
-                <CircularProgress disableShrink />
-              </div>
-            )} */}
+            <div className="w-full flex justify-center">
+            <CircularProgress disableShrink />
+            </div>
+          )} */}
           {/* </div> */}
         </div>
       </div>
@@ -259,74 +258,85 @@ export default function HomePage() {
 // import React, { useRef, useEffect } from 'react';
 
 // function ScrollableDivComponent({ onScrollToEnd }) {
-//   const scrollableDivRef = useRef();
-
-//   useEffect(() => {
-//     const handleScroll = () => {
-//       const scrollableDiv = scrollableDivRef.current;
-
-//       // Check if the user has scrolled to the bottom of the scrollable div
-//       if (
-//         scrollableDiv.scrollHeight - scrollableDiv.scrollTop ===
-//         scrollableDiv.clientHeight
-//       ) {
-//         // Call the provided callback function when reaching the end
-//         if (onScrollToEnd) {
-//           onScrollToEnd();
-//         }
-//       }
-//     };
-
-//     // Attach the event listener to the scrollable div
-//     const scrollableDiv = scrollableDivRef.current;
-//     if (scrollableDiv) {
-//       scrollableDiv.addEventListener('scroll', handleScroll);
-//     }
-
-//     // Detach the event listener on component unmount
-//     return () => {
-//       if (scrollableDiv) {
-//         scrollableDiv.removeEventListener('scroll', handleScroll);
-//       }
-//     };
-//   }, [onScrollToEnd]);
-
-//   return (
-//     <div
-//       ref={scrollableDivRef}
-//       style={{
-//         overflowY: 'scroll',
-//         height: '300px', // Set the desired height for the scrollable div
-//         border: '1px solid #ccc',
-//       }}
-//     >
-//       {/* Content of the scrollable div */}
-//       {/* ... */}
-//     </div>
-//   );
-// }
-
-/// page loding
-
-// const handelInfiniteScroll = async () => {
-//   // console.log("scrollHeight" + document.documentElement.scrollHeight);
-//   // console.log("innerHeight" + window.innerHeight);
-//   // console.log("scrollTop" + document.documentElement.scrollTop);
-//   try {
-//     window.alert('scrolled at end'+page)
-//     if (
-//       window.innerHeight + document.documentElement.scrollTop + 1 >=
+  //   const scrollableDivRef = useRef();
+  
+  //   useEffect(() => {
+    //     const handleScroll = () => {
+      //       const scrollableDiv = scrollableDivRef.current;
+      
+      //       // Check if the user has scrolled to the bottom of the scrollable div
+      //       if (
+        //         scrollableDiv.scrollHeight - scrollableDiv.scrollTop ===
+        //         scrollableDiv.clientHeight
+        //       ) {
+          //         // Call the provided callback function when reaching the end
+          //         if (onScrollToEnd) {
+            //           onScrollToEnd();
+            //         }
+            //       }
+            //     };
+            
+            //     // Attach the event listener to the scrollable div
+            //     const scrollableDiv = scrollableDivRef.current;
+            //     if (scrollableDiv) {
+              //       scrollableDiv.addEventListener('scroll', handleScroll);
+              //     }
+              
+              //     // Detach the event listener on component unmount
+              //     return () => {
+                //       if (scrollableDiv) {
+                  //         scrollableDiv.removeEventListener('scroll', handleScroll);
+                  //       }
+                  //     };
+                  //   }, [onScrollToEnd]);
+                  
+                  //   return (
+                    //     <div
+                    //       ref={scrollableDivRef}
+                    //       style={{
+                      //         overflowY: 'scroll',
+                      //         height: '300px', // Set the desired height for the scrollable div
+                      //         border: '1px solid #ccc',
+                      //       }}
+                      //     >
+                      //       {/* Content of the scrollable div */}
+                      //       {/* ... */}
+                      //     </div>
+                      //   );
+                      // }
+                      
+                      /// page loding
+                      
+                      // const handelInfiniteScroll = async () => {
+                        //   // console.log("scrollHeight" + document.documentElement.scrollHeight);
+                        //   // console.log("innerHeight" + window.innerHeight);
+                        //   // console.log("scrollTop" + document.documentElement.scrollTop);
+                        //   try {
+                          //     window.alert('scrolled at end'+page)
+                          //     if (
+                            //       window.innerHeight + document.documentElement.scrollTop + 1 >=
 //       document.documentElement.scrollHeight
 //     ) {
-//       // setLoading(true);
-//       setPage((prev) => prev + 1);
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// useEffect(() => {
-//   window.addEventListener("scroll", handelInfiniteScroll);
-//   return () => window.removeEventListener("scroll", handelInfiniteScroll);
-// }, []);
+  //       // setLoading(true);
+  //       setPage((prev) => prev + 1);
+  //     }
+  //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // };
+    
+    // useEffect(() => {
+      //   window.addEventListener("scroll", handelInfiniteScroll);
+      //   return () => window.removeEventListener("scroll", handelInfiniteScroll);
+      // }, []);
+      
+      // useEffect(() => {
+      //   if (checked.length || value.length) filterProduct();
+      // }, [checked, value]);
+      
+      //life cycle method
+      // useEffect(() => {
+      //   getTotal();
+      //   if (!checked.length){ getAllProducts()};
+      //   //eslint-disable-next-line
+      // }, []);
