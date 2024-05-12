15b23a0b872121f2062827f6918c8890bc642af5
thekeshavkeshari <desifighterrr@gmail.com>
2024-05-12 11:49:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,13 @@
 
 // export default app;
 import  express  from "express";
-import "dotenv/config";
+// import "dotenv/config";
 import morgan from "morgan";
-import connectDB from "./config/db.js";
-import authRoute from "./routes/authRoute.js"
+// import connectDB from "./config/db.js";
+// import authRoute from "./routes/authRoute.js"
 import cors from "cors"; 
-import categoryRoutes from './routes/categoryRoutes.js'
-import productRoute from './routes/productRoute.js'
+// import categoryRoutes from './routes/categoryRoutes.js'
+// import productRoute from './routes/productRoute.js'
 
 
 // connect database
@@ -37,9 +37,9 @@ app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'));
 
 //routes
-app.use('/api/v1/auth',authRoute);
-app.use('/api/v1/category',categoryRoutes);
-app.use('/api/v1/product',productRoute)
+// app.use('/api/v1/auth',authRoute);
+// app.use('/api/v1/category',categoryRoutes);
+// app.use('/api/v1/product',productRoute)
 app.use('/',(req,res)=>{
     res.send("hi bro backend is still alive");
 })
