4b9fb97f3609be8cc1a364fdfa9f2af4bd8ba717
thekeshavkeshari <desifighterrr@gmail.com>
2024-05-12 11:38:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
-import express from "express";
+// import express from "express";
 
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("00028532853");
+// });
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+// export default app;
+import  express  from "express";
+import "dotenv/config";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoute.js"
+import cors from "cors"; 
+import categoryRoutes from './routes/categoryRoutes.js'
+import productRoute from './routes/productRoute.js'
+
+
+// connect database
+
+connectDB();
+
+// rest object 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("00028532853");
+// middlewares 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(morgan('dev'));
+
+//routes
+app.use('/api/v1/auth',authRoute);
+app.use('/api/v1/category',categoryRoutes);
+app.use('/api/v1/product',productRoute)
+app.use('/',(req,res)=>{
+    res.send("hi bro backend is still alive");
+})
+
+const PORT = process.env.PORT||8080;
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`); 
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-export default app;
+export default app;    
