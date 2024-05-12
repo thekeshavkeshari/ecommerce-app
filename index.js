83d31ebfb5d29a65575eb7239d4f813c45d425c2
thekeshavkeshari <desifighterrr@gmail.com>
2024-05-12 11:36:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.send("00028532853");
 });
 
 const PORT = process.env.PORT || 5000;
