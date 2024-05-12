063c5664286a8e08fdec01d8655487fe31d707c7
thekeshavkeshari <desifighterrr@gmail.com>
2024-05-12 12:02:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 // middlewares 
 app.use(cors());
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 
 app.get("/", (req, res) => {
